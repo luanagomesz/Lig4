f7752a4d0d90d802a0a56fe970722a1fca33567c
luanagomesz <luanazangrandi@gmail.com>
2021-10-05 13:07:19 -0300

    verificação de vitoria diagonal

--- script.js
+++ script.js
@@ -80,6 +80,40 @@ function selecionar(e) {
             tabela.children[i].appendChild(criaDisco)            
         }        
     }
-  
+  vitoriaDiagonal()
 }
 
+
+//condição de vitoria diagonal
+
+function vitoriaDiagonal(){
+    let array = game.children
+      for(let i=0;i<array.length;i++){
+     
+        for(let k=0;k<array.length -3;k++){
+            //diagonal esquerda
+            cell = array[k].children[i]
+            if(cell.childElementCount !== 0){
+               cell = cell.lastElementChild
+
+               if(array[k+1].children[i+1].childElementCount !== 0 && array[k+2].children[i+2].childElementCount !== 0 && array[k+3].children[i+3].childElementCount !== 0){
+                 if(cell.classList.value == array[k+1].children[i+1].lastElementChild.classList.value  && cell.classList.value == array[k+2].children[i+2].lastElementChild.classList.value && cell.classList.value == array[k+3].children[i+3].lastElementChild.classList.value){
+                   console.log('vrauu' + " " + cell.classList.value + " " + "Ganhou")
+               }
+            }
+         }
+        }
+        for(let x = array.length -1;x>2;x--){
+            //diagonal pra direita
+            cellx = array[x].children[i]
+            if(cellx.childElementCount !== 0){
+                cellx = cellx.lastElementChild
+                if(array[x-1].children[i+1].childElementCount !== 0 && array[x-2].children[i+2].childElementCount !== 0 && array[x-3].children[i+3].childElementCount !== 0){
+                    if(cellx.classList.value == array[x-1].children[i+1].lastElementChild.classList.value  && cellx.classList.value == array[x-2].children[i+2].lastElementChild.classList.value && cellx.classList.value == array[x-3].children[i+3].lastElementChild.classList.value){
+                      console.log('vrauu' + " " + cellx.classList.value + " " + "Ganhou")
+                  }
+                 }
+                }
+             }
+          }
+         }
