4e64f65c23a181e29f6cb836ab8b8638bfa46d68
Marta Lima <martachmlima@gmail.com>
2021-10-06 15:23:19 -0300

    resolvendo conflito

--- script.js
+++ script.js
@@ -82,8 +82,10 @@ function selecionar(e) {
             tabela.children[i].appendChild(criaDisco)            
         }
     }  
+    empate()
     verificaH()
     verificaV()
+    vitoriaDiagonal()
 }
 
 // função verificar vitoria horizontal e vertical
@@ -107,37 +109,7 @@ function verificaH(){
         }
         }
     }
-<<<<<<< HEAD
-=======
-
-}
-
-function verificaV(){
-    let tabela = [...game.children]
-    for(let i = 0; i < tabela.length - 3; i++){
-        for(let j = 0; j < tabela.length ; j++) {
-          let cell = tabela[j].children[i]
-          if(cell === undefined)continue
-          let cellCount = cell.childElementCount
-          if(cellCount !== 0) {
-              let cell2 = tabela[j].children[i+1]
-              let cell3 = tabela[j].children[i+2]
-              let cell4 = tabela[j].children[i+3]
-            if((cell2.childElementCount !==0 && cell3.childElementCount !==0 && cell4.childElementCount !==0) &&
-                (cell.lastElementChild.classList.value === cell2.lastElementChild.classList.value &&
-                 cell.lastElementChild.classList.value === cell3.lastElementChild.classList.value &&
-                 cell.lastElementChild.classList.value === cell4.lastElementChild.classList.value) ) {
-              console.log("aleluiavertical");
-            }
-         }
-        }
-      }
-
->>>>>>> resetButton
-  empate()
-  verificaH()
-  verificaV()
-  vitoriaDiagonal()
+ 
 }
 
 
@@ -278,8 +250,5 @@ function vitoriaDiagonal(){
             
         }
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> resetButton
